d7dad8b3f781e535fc67f4d0b107f84cb5cb07f7
kiran <sai8kiran99@gmail.com>
2020-06-20 12:03:59 +0530

    fixed overflow pixel issue

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -16,102 +16,106 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "OU Notes",
-            style: TextStyle(
-              fontSize: 50,
-              color: Colors.white,
-              fontWeight: FontWeight.w200,
-            ),
-          ),
-          SizedBox(
-            height: 85,
-          ),
-          Card(
-            margin: EdgeInsets.all(30),
-            color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextFormField(
-                      controller: _emailFieldController,
-                      cursorColor: Colors.black,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        hintText: "Enter Email",
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.w300),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              const BorderSide(color: Colors.black, width: 2.0),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "OU Notes",
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
+              SizedBox(
+                height: 85,
+              ),
+              Card(
+                margin: EdgeInsets.all(30),
+                color: Colors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextFormField(
+                          controller: _emailFieldController,
+                          cursorColor: Colors.black,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: "Enter Email",
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                                color: Colors.black, fontWeight: FontWeight.w300),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.black, width: 2.0),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextFormField(
-                      controller: _passwordFieldController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Enter Password",
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.w300),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide:
-                              const BorderSide(color: Colors.black, width: 2.0),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextFormField(
+                          controller: _passwordFieldController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: "Enter Password",
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                                color: Colors.black, fontWeight: FontWeight.w300),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.black, width: 2.0),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Container(
-                  child: RaisedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "LOGIN",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w300,
+                    Container(
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "LOGIN",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        color: Colors.black,
+                        textColor: Colors.white,
                       ),
                     ),
-                    color: Colors.black,
-                    textColor: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 6),
-                Text("(or)"),
-                Container(
-                  child: RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RegisterPage()));
-                    },
-                    child: Text(
-                      "Register",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w300,
+                    SizedBox(height: 6),
+                    Text("(or)"),
+                    Container(
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterPage()));
+                        },
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        color: Colors.white,
+                        textColor: Colors.black,
                       ),
                     ),
-                    color: Colors.white,
-                    textColor: Colors.black,
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
