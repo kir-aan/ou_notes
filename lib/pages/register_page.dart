ae29ca8ddfa7f04a78076e06c5a985984d7a80f4
kiran <sai8kiran99@gmail.com>
2020-06-20 12:01:26 +0530

    Register Page Design Completed

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -8,8 +8,138 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "OU Notes",
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            SizedBox(height:40),
+            Text(
+              "Register",
+              style: TextStyle(
+                fontSize: 35,
+                color: Colors.white,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(30),
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        // controller: _emailFieldController,
+                        cursorColor: Colors.black,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          hintText: "Enter Name",
+                          labelText: "Name",
+                          labelStyle: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.w300),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.black, width: 2.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        // controller: _emailFieldController,
+                        cursorColor: Colors.black,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: "Enter Email",
+                          labelText: "Email",
+                          labelStyle: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.w300),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.black, width: 2.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        // controller: _passwordFieldController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter Password",
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.w300),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.black, width: 2.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        // controller: _passwordFieldController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Re-Enter Password",
+                          labelText: "Confirm password",
+                          labelStyle: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.w300),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.black, width: 2.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Container(
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RegisterPage()));
+                      },
+                      child: Text(
+                        "Register",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      color: Colors.black,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
