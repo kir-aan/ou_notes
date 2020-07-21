5f12195ed3f426cf3ce33c38df9fedbc5d5d164e
kiran <sai8kiran99@gmail.com>
2020-07-21 13:02:11 +0530

    Added Branches Page

---
+++ lib/pages/branches_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BranchesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: <Widget>[],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        childAspectRatio: 3 / 2,
+      ),
+    );
+  }
+}
