0a4e2516aa41abcf6cd5ee30621bd6341857f158
kiran <sai8kiran99@gmail.com>
2020-07-21 18:37:40 +0530

    Appbar title is selected BranchName

--- lib/pages/semesters_page.dart
+++ lib/pages/semesters_page.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 class SemesterPage extends StatelessWidget {
+  final String branchID;
+  final String branchName;
+  SemesterPage(this.branchID, this.branchName);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("BranchName"),
+        title: Text(branchName),
       ),
       body: Center(
-        child: Text("Semesters"),
+        child: Text('${branchName} Selected'),
       ),
     );
   }
