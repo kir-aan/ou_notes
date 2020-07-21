9db2c4893dca4088739a3fc16a10c00e36396ddf
kiran <sai8kiran99@gmail.com>
2020-07-21 13:11:34 +0530

    dummyBranch Data added

---
+++ lib/models/dummyBranchesData.dart
@@ -0,0 +1,11 @@
+import './branch.dart';
+
+const DUMMY_BRANCHES = const [
+  Branch(id: 'b1', title: "IT"),
+  Branch(id: 'b2', title: "CSE"),
+  Branch(id: 'b3', title: "CIVIL"),
+  Branch(id: 'b4', title: "MECH"),
+  Branch(id: 'b5', title: "AUTO"),
+  Branch(id: 'b6', title: "EEE"),
+  Branch(id: 'b7', title: "ECE"),
+];
