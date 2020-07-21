64042937aeb698708669568c4b5d9a4ccf447e88
kiran <sai8kiran99@gmail.com>
2020-07-21 19:57:52 +0530

    Added Semester Gridview

--- lib/pages/semesters_page.dart
+++ lib/pages/semesters_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/semseter_item.dart';
 
 class SemesterPage extends StatelessWidget {
   final String branchID;
@@ -11,8 +12,16 @@ class SemesterPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(branchName),
       ),
-      body: Center(
-        child: Text('${branchName} Selected'),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        children:
+            [1, 2, 3, 4, 5, 6, 7, 8].map((sem) => SemesterItem(sem)).toList(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 40,
+          crossAxisSpacing: 20,
+          childAspectRatio: 3 / 2,
+        ),
       ),
     );
   }
