a869fb058ab4b4e106a3a571735a7a47c905e2da
kiran <sai8kiran99@gmail.com>
2020-07-21 18:23:49 +0530

    Added Navigation to Semester Page

--- lib/models/branch_items.dart
+++ lib/models/branch_items.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
+import '../pages/semesters_page.dart';
 
 class BranchItem extends StatelessWidget {
   final String title;
   BranchItem(this.title);
+
+  void selectBranch(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return SemesterPage();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline1,
-        textAlign: TextAlign.center,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            Theme.of(context).accentColor.withOpacity(0.7),
-            Theme.of(context).accentColor,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectBranch(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline1,
+          textAlign: TextAlign.center,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              Theme.of(context).accentColor.withOpacity(0.7),
+              Theme.of(context).accentColor,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
