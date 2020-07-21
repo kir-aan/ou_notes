7af47161b33dcc0cd99d7a74d9355cf9ea603fb5
kiran <sai8kiran99@gmail.com>
2020-07-21 18:23:22 +0530

    Semster Page added

---
+++ lib/pages/semesters_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SemesterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BranchName"),
+      ),
+      body: Center(
+        child: Text("Semesters"),
+      ),
+    );
+  }
+}
