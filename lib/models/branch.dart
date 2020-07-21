1d070bba1798833e0420dc2581abcaa44025ea31
kiran <sai8kiran99@gmail.com>
2020-07-21 18:35:37 +0530

    renamed title to name

--- lib/models/branch.dart
+++ lib/models/branch.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class Branch {
   final String id;
-  final String title;
-  const Branch({@required this.id, @required this.title});
+  final String name;
+  const Branch({@required this.id, @required this.name});
 }
--- lib/models/dummyBranchesData.dart
+++ lib/models/dummyBranchesData.dart
@@ -1,11 +1,11 @@
 import './branch.dart';
 
 const DUMMY_BRANCHES = const [
-  Branch(id: 'b1', title: "IT"),
-  Branch(id: 'b2', title: "CSE"),
-  Branch(id: 'b3', title: "CIVIL"),
-  Branch(id: 'b4', title: "MECH"),
-  Branch(id: 'b5', title: "AUTO"),
-  Branch(id: 'b6', title: "EEE"),
-  Branch(id: 'b7', title: "ECE"),
+  Branch(id: 'b1', name: "IT"),
+  Branch(id: 'b2', name: "CSE"),
+  Branch(id: 'b3', name: "CIVIL"),
+  Branch(id: 'b4', name: "MECH"),
+  Branch(id: 'b5', name: "AUTO"),
+  Branch(id: 'b6', name: "EEE"),
+  Branch(id: 'b7', name: "ECE"),
 ];
