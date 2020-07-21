1de98a39a32d7c9d43f2e26ed003c6dd7419aadb
kiran <sai8kiran99@gmail.com>
2020-07-21 19:55:52 +0530

    Modified Color scheme and added new headline

--- lib/main.dart
+++ lib/main.dart
@@ -12,23 +12,29 @@ class MyApp extends StatelessWidget {
       title: 'OU Notes',
       theme: ThemeData(
         primaryColor: Colors.black,
-        primarySwatch: Colors.grey,
-        accentColor: Colors.blueGrey,
+        splashColor: Colors.black,
+        cardColor: Color.fromRGBO(197, 219, 212, 1),
+        accentColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'RobotoCondensed',
         textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(
-                color: Colors.green[900],
-              ),
-              bodyText2: TextStyle(
-                color: Colors.blueGrey[700],
-              ),
-              headline1: TextStyle(
-                fontSize: 50,
-                fontFamily: 'RobotoCondensed',
-                fontWeight: FontWeight.w700,
-              ),
+            bodyText1: TextStyle(
+              color: Colors.green[900],
             ),
+            bodyText2: TextStyle(
+              color: Colors.blueGrey[700],
+            ),
+            headline1: TextStyle(
+              fontSize: 50,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.w700,
+            ),
+            headline2: TextStyle(
+              fontSize: 40,
+              color: Colors.grey[850],
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.w300,
+            )),
       ),
       home: BranchesPage(),
     );
