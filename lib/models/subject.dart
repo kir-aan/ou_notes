3f7581329bd8d999e9ff11ca91b7b77e3d461ae1
kiran <sai8kiran99@gmail.com>
2020-07-23 19:42:09 +0530

    branchID,semester,subjectID added

--- lib/models/subject.dart
+++ lib/models/subject.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/foundation.dart';
 
 class Subject {
-  final String id;
+  final String branchID;
+  final int semester;
+  final String subjectID;
   final String subjectName;
   final List<String> subjectNotes;
   const Subject({
-    @required this.id,
+    @required this.branchID,
+    @required this.semester,
+    @required this.subjectID,
     @required this.subjectName,
     @required this.subjectNotes,
   });
