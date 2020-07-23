67d50ec1af03ebc8b94d98e20b07a13c3f514e21
kiran <sai8kiran99@gmail.com>
2020-07-23 20:23:12 +0530

    logic for retrieving subjects done.

--- lib/pages/subjects_page.dart
+++ lib/pages/subjects_page.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 import '../models/dummyData.dart';
 
 class SubjectsPage extends StatelessWidget {
+  final int semester;
+  final String branchID;
+  // final List<String> subjectNotes;
+  SubjectsPage({this.semester, this.branchID});
+
   @override
   Widget build(BuildContext context) {
+    final subjects = DUMMY_SUBJECTS
+        .where(
+            (sub) => (sub.branchID == branchID) && (sub.semester == semester))
+        .toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text("SubjectName"),
+        title: Text("SEM-$semester"),
       ),
-      body: Text("Subject Page"),
+      body: Center(child: Text("${subjects[0].subjectName}")),
     );
   }
 }
