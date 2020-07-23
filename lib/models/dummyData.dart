35a7958e2d241843a5c2ecfa70f628fdbd692892
kiran <sai8kiran99@gmail.com>
2020-07-23 19:21:46 +0530

    dummy subjects data added

--- lib/models/dummyData.dart
+++ lib/models/dummyData.dart
@@ -1,4 +1,5 @@
 import 'branch.dart';
+import 'subject.dart';
 
 const DUMMY_BRANCHES = const [
   Branch(id: 'b1', name: "IT"),
@@ -8,4 +9,40 @@ const DUMMY_BRANCHES = const [
   Branch(id: 'b5', name: "AUTO"),
   Branch(id: 'b6', name: "EEE"),
   Branch(id: 'b7', name: "ECE"),
+];
+
+const DUMMY_SUBJECTS = const [
+  Subject(
+    id: 'sub1',
+    subjectName: 'SubjectName1',
+    subjectNotes: [
+      "Unit-1.pdf",
+      "Unit-2.pdf",
+      "Unit-3.pdf",
+      "Unit-4.pdf",
+      "Unit-5.pdf",
+    ],
+  ),
+  Subject(
+    id: 'sub2',
+    subjectName: 'SubjectName2',
+    subjectNotes: [
+      "Unit-1.pdf",
+      "Unit-2.pdf",
+      "Unit-3.pdf",
+      "Unit-4.pdf",
+      "Unit-5.pdf",
+    ],
+  ),
+  Subject(
+    id: 'sub3',
+    subjectName: 'SubjectName3',
+    subjectNotes: [
+      "Unit-1.pdf",
+      "Unit-2.pdf",
+      "Unit-3.pdf",
+      "Unit-4.pdf",
+      "Unit-5.pdf",
+    ],
+  ),
 ];
