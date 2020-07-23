89c3ba02256a92587c0d7303b4718c5ee212ee23
kiran <sai8kiran99@gmail.com>
2020-07-23 19:52:53 +0530

    basic subject page

---
+++ lib/pages/subjects_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../models/dummyData.dart';
+
+class SubjectsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SubjectName"),
+      ),
+      body: Text("Subject Page"),
+    );
+  }
+}
